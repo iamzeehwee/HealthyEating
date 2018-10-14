b014cbcf50703bb48433810cf2725fdf998c04d4
Artem Chernyshov <artyom.chernishov@gmail.com>
2018-10-14 16:41:24 +0000

    Favourites can be displayed now

--- app/src/main/java/com/example/healthyeating/healthyeating/Boundary/FavouriteFragment.java
+++ app/src/main/java/com/example/healthyeating/healthyeating/Boundary/FavouriteFragment.java
@@ -3,35 +3,26 @@ package com.example.healthyeating.healthyeating.Boundary;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import com.example.healthyeating.healthyeating.Controller.LocationsManager;
-import com.example.healthyeating.healthyeating.Entity.HealthyLocation;
 import com.example.healthyeating.healthyeating.R;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavouriteFragment extends Fragment {
+    private static final String TAG = "Favourite";
 
-    ListView favouriteListView;
+    // declare the layout elements
+    private Spinner categorySpinner;
+    private TextView categoryTextView;
+    private ListView favouritesView;
 
     public FavouriteFragment() {
         // Required empty public constructor
@@ -41,37 +32,15 @@ public class FavouriteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_favourite, container, false);
+
+        categorySpinner = (Spinner) view.findViewById(R.id.categorySpinner);
+        categoryTextView = (TextView) view.findViewById(R.id.categoryTextView);
+        favouritesView = (ListView) view.findViewById(R.id.favouritesView);
+
         // Inflate the layout for this fragment
-
-
-        return inflater.inflate(R.layout.fragment_favourite, container, false);
+        return view;
     }
 
-    public ArrayList<HealthyLocation> getFavourites(){
-
-        LocationsManager locationsManager = ((MainActivity)getActivity()).lm;
-        ArrayList<HealthyLocation> healthyLocations = new ArrayList<>();
-        String filename = "FavouriteListData";
-
-        //read file
-        FileInputStream inputStream;
-        try{
-            inputStream = getContext().openFileInput(filename);
-            BufferedReader bR = new BufferedReader(new InputStreamReader(inputStream));
-            ArrayList<Integer> saved = new ArrayList<>();
-            for (String line; (line = bR.readLine()) != null; ) {
-                int id = Integer.parseInt(line);
-                HealthyLocation healthyLocation = locationsManager.getLocation(id);
-                healthyLocations.add(healthyLocation);
-            }
-            bR.close();
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return healthyLocations;
-    }
 }
