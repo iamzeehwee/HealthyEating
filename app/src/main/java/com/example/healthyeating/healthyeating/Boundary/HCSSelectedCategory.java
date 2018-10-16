3366d01ffe836409889e3b1ec9709d21499f1c33
weimock <mockjinwei94@gmail.com>
2018-10-16 12:54:53 +0800

    Added: Layout for showing food product from selected category To Do: Working on displaying the relevant data into layout

---
+++ app/src/main/java/com/example/healthyeating/healthyeating/Boundary/HCSSelectedCategory.java
@@ -0,0 +1,15 @@
+package com.example.healthyeating.healthyeating.Boundary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.healthyeating.healthyeating.R;
+
+public class HCSSelectedCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hcs__selected__category);
+    }
+}
